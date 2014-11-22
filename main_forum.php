ae7859404b731179adc7a1df336b8375b498c46d
Ravi <kumarravi.k78c@gmail.com>
2014-11-22 15:56:11 -0500

    Modified main_forum class to show data

--- main_forum.php
+++ main_forum.php
@@ -1,21 +1,15 @@
 <?php
 
-$host="localhost"; // Host name 
+
 $username="root"; // Mysql username 
 $password=""; // Mysql password 
 $db_name="eduvisor"; // Database name 
 $tbl_name="forum_question1"; // Table name 
 
-// Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
-$sql="SELECT * FROM $tbl_name ORDER BY id DESC";
-// OREDER BY id DESC is order result by descending
-
-$result=mysql_query($sql);
-?>
-<head>
-
+session_start();
+echo'
+<html lang="en">
+    <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -55,10 +49,9 @@ $result=mysql_query($sql);
         color:white;
     }
     </style>
+    </head>
 
-</head>
-<body id="page-top" class="index" style="background:#b00;">
-
+    <body id="page-top" class="index" style="background:#b00;">
     <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
@@ -71,17 +64,11 @@ $result=mysql_query($sql);
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="#page-top">Edu-Visor</a>
-            </div>
+            </div>';
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <?php
-session_start();
-if(isset($_SESSION['student_id'])){
-    $con = mysqli_connect("localhost","root","","eduvisor");
-        if(mysqli_connect_errno())
-            echo "Failed" . mysqli_connect_error();
-    $student_id=$_SESSION['student_id'];
-    echo '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            if(isset($_SESSION['student_id'])){
+            echo'
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="hidden">
                         <a href="#page-top"></a>
@@ -102,9 +89,9 @@ if(isset($_SESSION['student_id'])){
                     </li>-->
                 </ul>
             </div>';
-} else{
-    session_destroy();
-    echo '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            }else{
+            echo'    
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="hidden">
                         <a href="#page-top"></a>
@@ -126,8 +113,8 @@ if(isset($_SESSION['student_id'])){
                     </li>-->
                 </ul>
             </div>';
-}    
-?>
+            }
+            echo '
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container-fluid -->
@@ -135,87 +122,52 @@ if(isset($_SESSION['student_id'])){
     <header>
         <div class="row" style="padding-top:80px;">
             <h1>Edu-visor Community</h1>
-            <h4 style="text-transform:none;margin-top:30px;">If you can't find the answer using our search, try posting your question here to see answers from other users.</h4>
+            <hr class="star-light"/>
+            <h4 style="text-transform:none;margin-top:30px;">If you cant find the answer using our search, try posting your question here to see answers from other users.</h4>
         </div>
     </header>
-    <div class="row" style="margin-top:80px;">
-        <!--
-<table width="90%" border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
-<tr>
-<td width="6%" align="center" bgcolor="#E6E6E6"><strong>#</strong></td>
-<td width="53%" align="center" bgcolor="#E6E6E6"><strong>Topic</strong></td>
-<td width="15%" align="center" bgcolor="#E6E6E6"><strong>Views</strong></td>
-<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Replies</strong></td>
-<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Date/Time</strong></td>
-</tr>
--->
-<div class="col-md-10 col-md-offset-1">
-<table class="table table-striped table-bordered">
-    <tr>
-        <th class="col-md-5 text-center">
-            Topic
-        </th>
-        <th class="col-md-2 text-center">
-            Views
-        </th>
-        <th class="col-md-2 text-center">
-            Replies
-        </th>
-        <th class="col-md-3 text-center">
-            Posted
-        </th>
-    </tr>
-<?php
- 
-// Start looping table row
-while($rows=mysql_fetch_array($result)){
-?>
-<!--
-<tr>
-<td bgcolor="#FFFFFF"><? echo $rows['id']; ?></td>
-<td bgcolor="#FFFFFF"><a href="view_topic.php?id=<? echo $rows['id']; ?>"><? echo $rows['topic']; ?></a><BR></td>
-<td align="center" bgcolor="#FFFFFF"><? echo $rows['view']; ?></td>
-<td align="center" bgcolor="#FFFFFF"><? echo $rows['reply']; ?></td>
-<td align="center" bgcolor="#FFFFFF"><? echo $rows['datetime']; ?></td>
-</tr>
--->
-<tr>
-    <td class="col-md-5 text-center">
-        <a href="view_topic.php?id=<? echo $rows['id']; ?>"><? echo $rows['topic']; ?></a>
-    </td>
-    <td class="col-md-2 text-center">
-        <? echo $rows['view']; ?>
-    </td>
-    <td class="col-md-2 text-center">
-        <? echo $rows['reply']; ?>
-    </td>
-    <td class="col-md-3 text-center">
-        <? echo $rows['datetime']; ?>
-    </td>
-</tr>
+    <div class="row" style="margin-top:40px; margin-bottom:50px;">
+        <div class="col-md-10 col-md-offset-1">
+            <table class="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th class="col-md-5 text-center">Topic</th>
+                    <th class="col-md-2 text-center">Views</th>
+                    <th class="col-md-2 text-center">Replies</th>
+                    <th class="col-md-3 text-center">Posted</th>
+                </tr>
+                </thead>
+                <tbody>';
+                $con = mysqli_connect("localhost","root","","eduvisor");
+                if(mysqli_connect_errno())
+                        echo "Failed" . mysqli_connect_error(); 
 
-<?php
-// Exit looping and close connection 
-}
-mysql_close();
-?>
-</table>
-</div>
-<!--<td colspan="5" align="right" bgcolor="#E6E6E6"><a href="create_topic.php"><strong>Create New Topic</strong> </a></td>
-</tr>
-</table>-->
-</div>
-<?php
-if(isset($_SESSION['student_id'])){
-    $con = mysqli_connect("localhost","root","","eduvisor");
-        if(mysqli_connect_errno())
-            echo "Failed" . mysqli_connect_error();
-    $student_id=$_SESSION['student_id'];
-    echo '<div class="row col-md-2 col-md-offset-9" style="margin-top:40px;">
+                $result = mysqli_query($con,"SELECT * FROM $tbl_name ORDER BY id DESC");
+
+                while($row=mysqli_fetch_array($result)){
+                echo'
+                    <tr>
+                        <td><a href="view_topic.php?id='.$row['id'].'">'.$row['topic'].'</a></td>
+                        <td class="col-md-2 text-center">'.$row['view'].'</td>
+                        <td class="col-md-2 text-center">'.$row['reply'].'</td>
+                        <td class="col-md-3 text-center">'.$row['datetime'].'</td>
+                    </tr>';
+                }
+                echo'
+                </tbody>
+            </table>
+        </div>';
+        if(isset($_SESSION['student_id'])){
+        echo'    
+        <div class="row col-md-2 col-md-offset-9" style="margin-top:40px;">
         <a type="button" class="btn btn-primary btn-md btn-block" href="create_topic.php">Add new topic</a>
-    </div>';
-} ?>
-<footer class="text-center navbar-fixed-bottom" id="footer">
+        </div>';
+        }
+    echo'    
+    </div>
+    </body>
+
+    <footer class="text-center" id="footer">
         <div class="footer-below">
             <div class="container">
                 <div class="row">
@@ -226,4 +178,5 @@ if(isset($_SESSION['student_id'])){
             </div>
         </div>
     </footer>
-</body>
+</html>';
+?> 
